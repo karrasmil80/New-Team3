0a8cb1f0ca4e26ae7ca1ac63b51b148ac175b7e0
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-05-21 18:01:58 +0200

    añadido el result

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module dev.newteam.newteam3 {
     requires javafx.controls;
     requires javafx.fxml;
     requires kotlin.stdlib;
+    requires kotlin.result.jvm;
 
 
     opens dev.newteam.newteam3 to javafx.fxml;

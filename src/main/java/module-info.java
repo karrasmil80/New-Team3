a6adda06b46f7eafd542c5373644aaf463773abf
Pablo Zuil <zupablo3@gmail.com>
2025-05-22 13:37:08 +0200

    Configuracion ajustada

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -15,11 +15,19 @@ module dev.newteam.newteam3 {
 
     //KOIN-
     requires koin.core.jvm;
+    requires org.jdbi.v3.core;
+    requires org.jdbi.v3.sqlobject;
+    requires org.jdbi.v3.kotlin;
+    requires com.github.benmanes.caffeine;
 
 
     //CARPETAS A ABRIR EN LA APP
     //APP
     opens dev.newteam.newteam3 to javafx.fxml;
     exports dev.newteam.newteam3 to javafx.graphics;
+
+    //JDBI MANAGER
+    opens dev.newteam.newteam3.database to javafx.fxml;
+    exports dev.newteam.newteam3.database to javafx.graphics;
 
 }

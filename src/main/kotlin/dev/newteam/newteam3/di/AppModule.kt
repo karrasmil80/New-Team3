1801780a974f236d3ef606cf913356987fc6cc43
Pablo Zuil <zupablo3@gmail.com>
2025-05-22 13:39:39 +0200

    Koin en proceso y documentado

--- src/main/kotlin/dev/newteam/newteam3/di/AppModule.kt
+++ src/main/kotlin/dev/newteam/newteam3/di/AppModule.kt
@@ -1,19 +1,57 @@
 package dev.newteam.newteam3.di
 
+import com.github.benmanes.caffeine.cache.Cache
 import dev.newteam.newteam3.config.Config
+import dev.newteam.newteam3.database.JdbiManager
+import dev.newteam.newteam3.players.models.Persona
+import dev.newteam.newteam3.players.utils.provideCacheCaffeine
+import dev.newteam.newteam3.players.utils.provideDatabaseManager
+import org.jdbi.v3.core.Jdbi
 import org.koin.dsl.module
 import org.lighthousegames.logging.logging
+import kotlin.math.sin
 
+//Logger
 private val logger = logging()
 
 val AppModule = module {
 
+    //Lo hacemos todo con try catch para no interrumpir el programa y saber cuando falla
+
+    /**
+     * Crea un singleton de [Config]
+     */
     try {
         single { Config() }
     } catch (e : Exception) {
         println(e)
         logger.error { "No se ha podido crear un singleton de la ${Config()}" }
-
     }
+
+    /**
+     * Proporciona el [JdbiManager]
+     */
+
+    try {
+        single<Jdbi> { provideDatabaseManager(
+            config = get()
+        ) }
+    } catch (e : Exception) {
+        println(e)
+        logger.error { "No se ha podido proporcionar un singleton del manager}" }
+    }
+
+    /**
+     * Proporciona la cache [Caffeine]
+     */
+
+    try {
+        single<Cache<Int, Persona>> { provideCacheCaffeine() }
+    } catch (e : Exception) {
+        println(e)
+        logger.error { "No se ha podido proporcionar la cache caffeine" }
+    }
+
+
 
 }

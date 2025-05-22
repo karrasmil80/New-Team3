f7d18683e42171e57c42f0e30af6d3fdf2ec71d0
Pablo Zuil <zupablo3@gmail.com>
2025-05-22 12:17:25 +0200

    Configuracion del programa

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,9 +1,25 @@
 module dev.newteam.newteam3 {
+
+    //DEPENDENCIAS
+    //JAVA
     requires javafx.controls;
     requires javafx.fxml;
+
+    //KOTLIN
     requires kotlin.stdlib;
+    requires kotlin.reflect;
+
+    //LOGGER
+    requires logging.jvm;
+    requires org.slf4j;
+
+    //KOIN-
+    requires koin.core.jvm;
 
 
+    //CARPETAS A ABRIR EN LA APP
+    //APP
     opens dev.newteam.newteam3 to javafx.fxml;
-    exports dev.newteam.newteam3;
+    exports dev.newteam.newteam3 to javafx.graphics;
+
 }

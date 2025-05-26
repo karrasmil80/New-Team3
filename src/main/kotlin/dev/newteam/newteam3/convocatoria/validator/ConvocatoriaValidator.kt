e8c781b3794a8c67146c871c62d0bb0266f2e296
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-05-26 18:50:50 +0200

    validador convocatoria creado

---
+++ src/main/kotlin/dev/newteam/newteam3/convocatoria/validator/ConvocatoriaValidator.kt
@@ -0,0 +1,12 @@
+package dev.newteam.newteam3.convocatoria.validator
+
+import com.github.michaelbull.result.Result
+import dev.newteam.newteam3.convocatoria.models.Convocatoria
+import dev.newteam.newteam3.plantilla.error.PersonaError
+import dev.newteam.newteam3.plantilla.validator.Validator
+
+class ConvocatoriaValidator: Validator<Convocatoria, PersonaError> {
+    override fun validate(t: Convocatoria): Result<Convocatoria, PersonaError> {
+
+    }
+}

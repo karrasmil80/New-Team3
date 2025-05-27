f31e32b4ae52c26b2fcb6d9f7e2a99307f46aaa0
Nicolás Ortega Fernández <nicolas.ortega4@educa.madrid.org>
2025-05-27 16:28:42 +0200

    contraseña hash

--- src/main/kotlin/dev/newteam/newteam3/controller/LoginController.kt
+++ src/main/kotlin/dev/newteam/newteam3/controller/LoginController.kt
@@ -1,4 +1,15 @@
 package dev.newteam.newteam3.controller
 
+import org.mindrot.jbcrypt.BCrypt
+
 class LoginController {
+    fun login(username: String, password: String): Boolean {
+        val correctUsername = "admin"
+        val hashedPassword = "\$2a\$12\$nb2drgFJPylkW9PfeQ01oeI0SFEH/XdBKMhJNpmLpACMqeTvVLUz2"
+
+        // user:      admin
+        // password:  contra123
+
+        return username == correctUsername && BCrypt.checkpw(password, hashedPassword)
+    }
 }
